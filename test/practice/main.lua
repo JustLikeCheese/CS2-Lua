5dc667c505e7429024f5c8bbf0cf37d50c3f4ffa
JustLikeCheese <854675824@qq.com>
2025-10-21 15:08:44 +0800

    test: 更新跑图脚本

--- test/practice/main.lua
+++ test/practice/main.lua
@@ -1,4 +1,3 @@
-package.path = package.path .. ";C:/Users/Cheese/Desktop/CS2-Lua/src/?.lua"
 require "cs2"
 cs2.config {
     mp_buy_anywhere = 1, -- 允许任意位置购买
@@ -6,11 +5,13 @@ cs2.config {
     ammo_grenade_limit_total = 5, -- 可以购买五颗手雷这样诱饵弹也可以买了
     mp_buytime = 99999999, -- 无限购买时间
     sv_infinite_ammo = 1, -- 无限子弹及投掷物
-    -- 兼容 CFG
-    "sv_showimpacts 0", -- 显示子弹落点
-    "cl_showfps 0", -- 显示帧数
+    sv_showimpacts = 0, -- 显示子弹落点
+    cl_showfps = 0, -- 显示帧数
+    mp_drop_knife_enable = 1, -- 允许丢刀
+    mp_weapons_glow_on_ground = 1, -- 武器掉落高亮显示
 }
-cs2.clear()
 cs2.bind("x", "noclip") -- X 键飞行
+cs2.bind("k", "sv_rethrow_last_grenade") -- 重现服务器内上一个使用过的投掷物
+cs2.bind("l", "subclass_change 515") -- 蝴蝶刀
 cs2.print("跑图脚本已加载") -- 控制台输出消息
 cs2.build() -- 构建 CFG
