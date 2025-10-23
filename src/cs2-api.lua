460d752f1081fa8242cf215edd45ac19d7eceb00
JustLikeCheese <854675824@qq.com>
2025-10-23 13:44:25 +0800

    feat: 优化 CS2 API 接口

--- src/cs2-api.lua
+++ src/cs2-api.lua
@@ -1,9 +1,13 @@
+-- Counter-Strike 2 API
+-- 提供 CS2 API 的封装
+
+local cfg = cs2.cfg
 --- 绑定按键
 --- Bind key
 --- @param key string
 --- @param command any
 function cs2.bind(key, command)
-    cs2.func("bind", cs2.cfg.formatString(key), cs2.cfg.formatArg(command))
+    cs2.func("bind", cfg.formatString(key), cfg.formatArg(command))
 end
 
 --- 创建别名
@@ -11,19 +15,19 @@ end
 --- @param key string
 --- @param command any
 function cs2.alias(key, command)
-    cs2.func("alias", cs2.cfg.formatString(key), cs2.cfg.formatArg(command))
+    cs2.func("alias", cfg.formatString(key), cfg.formatArg(command))
 end
 
 --- 输出文本
 --- Print text
 function cs2.print(text)
-    cs2.func("echo", cs2.cfg.formatString(text))
+    cs2.func("echo", cfg.formatString(text))
 end
 
 --- 输出文本
 --- Print text
 function cs2.println(text)
-    cs2.func("echoln", cs2.cfg.formatString(text))
+    cs2.func("echoln", cfg.formatString(text))
 end
 
 --- 清空 CS2 控制台输出
@@ -36,13 +40,13 @@ end
 --- Run CS2 script
 function cs2.exec(path)
     if type(path) == "string" then
-        cs2.func("exec", path)
+        cs2.func("exec", cfg.formatString(path))
     elseif type(path) == "table" then
         for _, v in ipairs(path) do
-            cs2.func("exec", v)
+            cs2.exec(v)
         end
     else
-        error("Invalid path")
+        cs2.core.error("Invalid argument type: " .. type(path))
     end
 end
 
