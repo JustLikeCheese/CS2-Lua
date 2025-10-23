4c56ea247c1d8a7e89c88cb69cb16a2dbad98086
JustLikeCheese <854675824@qq.com>
2025-10-23 13:42:48 +0800

    fix: 修复 CS2 API 接口

--- src/cs2-api.lua
+++ src/cs2-api.lua
@@ -1,40 +1,3 @@
---- 加载 CFG 配置
---- Load CS2 cfg
---- @param arg any
-function cs2.config(arg)
-    -- 优先按顺序处理数组部分
-    for i = 1, #arg do
-        local value = arg[i]
-        local value_type = type(value)
-        if value_type == "string" then
-            cs2.run(value)
-        elseif value_type == "table" then
-            cs2.config(value)
-        end
-    end
-    -- 再处理键值对部分
-    for key, value in pairs(arg) do
-        if type(key) == "string" then
-            local value_type = type(value)
-            if value_type == "string" or value_type == "number" or value_type == "boolean" then
-                cs2.func(key, vstr(value))
-            else
-                cs2.core.error("Invalid value type for key '" .. key .. "': " .. value_type)
-            end
-        end
-    end
-end
-
---- 运行 CS2 命令
---- Run CS2 command
---- @param command string
-function cs2.run(command)
-    local cfg = cs2.cfg.parse(command)
-    for _, v in ipairs(cfg) do
-        cs2.add(v)
-    end
-end
-
 --- 绑定按键
 --- Bind key
 --- @param key string
@@ -45,20 +8,22 @@ end
 
 --- 创建别名
 --- Create alias
+--- @param key string
+--- @param command any
 function cs2.alias(key, command)
-    cs2.func("alias", key, command)
+    cs2.func("alias", cs2.cfg.formatString(key), cs2.cfg.formatArg(command))
 end
 
 --- 输出文本
 --- Print text
 function cs2.print(text)
-    cs2.func("echo", text)
+    cs2.func("echo", cs2.cfg.formatString(text))
 end
 
 --- 输出文本
 --- Print text
 function cs2.println(text)
-    cs2.func("echoln", text)
+    cs2.func("echoln", cs2.cfg.formatString(text))
 end
 
 --- 清空 CS2 控制台输出
@@ -83,5 +48,5 @@ end
 
 -- 触发 CS2 事件
 function cs2.event(name)
-    cs2.add(name)
+    cs2.run(name)
 end
--- src/cs2.lua
+++ src/cs2.lua
@@ -27,6 +27,43 @@ function cs2.func(funcName, ...)
     cs2.add({ funcName, ... })
 end
 
+--- 加载 CFG 配置
+--- Load CS2 cfg
+--- @param arg any
+function cs2.config(arg)
+    -- 优先按顺序处理数组部分
+    for i = 1, #arg do
+        local value = arg[i]
+        local value_type = type(value)
+        if value_type == "string" then
+            cs2.run(value)
+        elseif value_type == "table" then
+            cs2.config(value)
+        end
+    end
+    -- 再处理键值对部分
+    for key, value in pairs(arg) do
+        if type(key) == "string" then
+            local value_type = type(value)
+            if value_type == "string" or value_type == "number" or value_type == "boolean" then
+                cs2.func(key, vstr(value))
+            else
+                cs2.core.error("Invalid value type for key '" .. key .. "': " .. value_type)
+            end
+        end
+    end
+end
+
+--- 运行 CS2 命令
+--- Run CS2 command
+--- @param command string
+function cs2.run(command)
+    local cfg = cs2.cfg.parse(command)
+    for _, v in ipairs(cfg) do
+        cs2.add(v)
+    end
+end
+
 require "cs2-cfg"
 require "cs2-core"
 require "cs2-build"
